137be9c40be9d067a6d4c09a4c91d2f827d49e5a
kenyaorozco <109471756+kenyaorozco@users.noreply.github.com>
2022-09-26 11:03:23 -0700

    Add files via upload

---
+++ First java Program/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+
+    public static void main(String[] args){
+        System.out.println("My name is kenya");
+        System.out.println("I am 22 yrs old");
+        System.out.println("My hometown is San Jose, CA");
+    }
+}
